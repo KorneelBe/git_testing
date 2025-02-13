9fe539d82677177750eaf0fbb8cdec5c2922f56c
KorneelBe <korneel.bernaert@gmail.com>
2025-02-13 14:59:35 +0100

    Revert "quick fix"
    This reverts commit 0a74bfb091d69ffb13ff06dfcb13d6d5696a7b0c.

--- test_2.py
+++ test_2.py
@@ -1,4 +1,8 @@
 print("another hello world ?")
+<<<<<<< HEAD
 print('another thing')
 print('do you know other commands ?')
 print('this wont fix anything')
+=======
+print('another thing')
+>>>>>>> parent of 0a74bfb (quick fix)
